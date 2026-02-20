5a0e2829743eaa9623c75116c40e125dd68e5a7e
Emre Kayikcilar <emreebrukayikcilar@gmail.com>
2026-02-20 17:26:04 +0300

    publish script has been updated

--- publish.ps1
+++ publish.ps1
@@ -6,10 +6,10 @@ if (Test-Path "./publish") {
 # Run dotnet publish
 dotnet publish -c Release -f net8.0 -o ./publish
 
-# Remove old zip if it exists
-if (Test-Path "./publish/publish.zip") {
-    Remove-Item "./publish/publish.zip" -Force
+# Remove old tar/zip if it exists
+if (Test-Path "./publish/publish.tar") {
+    Remove-Item "./publish/publish.tar" -Force
 }
 
-# Create the zip file from publish contents
-Compress-Archive -Path "./publish/*" -DestinationPath "./publish/publish.zip"
+# Create the tar file from publish contents
+tar -cvf ./publish/publish.tar -C ./publish .
