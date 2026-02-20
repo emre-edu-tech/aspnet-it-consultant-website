26e9b661dfebf3b47045cf4017689a646c71e5e1
Emre Kayikcilar <emreebrukayikcilar@gmail.com>
2026-02-20 20:10:09 +0300

    README.md file updated, publish script has been updated using tar instead of Compress-Archive

--- publish.ps1
+++ publish.ps1
@@ -1,15 +1,20 @@
+# Variables
 # Remove publish folder if it exists
-if (Test-Path "./publish") {
-    Remove-Item "./publish" -Recurse -Force
+$publishDir = "./publish"
+$tarFile = "$publishDir/publish.tar"
+
+# Remove publish folder if it exists
+if (Test-Path $publishDir) {
+    Remove-Item $publishDir -Recurse -Force
 }
 
-# Run dotnet publish
-dotnet publish -c Release -f net8.0 -o ./publish
+# Run dotnet publish and recreate the publish folder with the published files inside 
+dotnet publish -c Release -f net8.0 -o $publishDir
 
 # Remove old tar/zip if it exists
-if (Test-Path "./publish/publish.tar") {
-    Remove-Item "./publish/publish.tar" -Force
+if (Test-Path $tarFile) {
+    Remove-Item $tarFile -Force
 }
 
 # Create the tar file from publish contents
-tar -cvf ./publish/publish.tar -C ./publish .
+tar -cvf $tarFile -C $publishDir .
